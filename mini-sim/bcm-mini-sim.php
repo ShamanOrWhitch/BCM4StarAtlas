4fb3951598bc629019c5af4ed0b3c1ae1b531fc2
Jah Man <131708486+ShamanOrWhitch@users.noreply.github.com>
2026-09-22 15:31:36 +0300

    Bump playable test to 0.2.4

--- mini-sim/bcm-mini-sim.php
+++ mini-sim/bcm-mini-sim.php
@@ -2,14 +2,14 @@
 /**
  * Plugin Name: BCM Mini Space Simulation
  * Description: Lightweight Descent-style 6DOF space-labyrinth simulation for WordPress.
- * Version: 0.2.3
+ * Version: 0.2.4
  * Author: ShamanOrWitch
  * License: GPL-2.0-or-later
  */
 
 if (!defined('ABSPATH')) exit;
 
-define('BCM_MINI_SIM_VERSION', '0.2.3');
+define('BCM_MINI_SIM_VERSION', '0.2.4');
 define('BCM_MINI_SIM_URL', plugin_dir_url(__FILE__));
 define('BCM_MINI_SIM_PATH', plugin_dir_path(__FILE__));
 
